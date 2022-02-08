dc739ad67b03c1d164057e0fd4ff3c6cef1f3476
Amethyst Willia <zmithic@gmail.com>
2022-02-08 12:42:08 -0600

    Added get request for all posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,4 +3,15 @@ const { Router } = require('express');
 const router = Router();
 const Post = require('./posts-model');
 
+// [GET] requests
+router.get('/', (req, res) => {
+    Post.find()
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(() => {
+            res.status(500).json({ message: "The posts information could not be retrieved" });
+        });
+});
+
 module.exports = router;
