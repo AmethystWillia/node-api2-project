c555c7d5f5b93fe13c9c41b5dc7ac9a2ebfcbaba
Amethyst Willia <zmithic@gmail.com>
2022-02-08 12:48:33 -0600

    Added get request for specific post ids

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -14,4 +14,19 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Post.findById(id)
+        .then(post => {
+            if (post === null || post === undefined) {
+                res.status(404).json({ message: "The post with the specified ID does not exist" });
+            } else {
+                res.status(200).json(post);
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ message: "The post information could not be retrieved" });
+        });
+});
+
 module.exports = router;
