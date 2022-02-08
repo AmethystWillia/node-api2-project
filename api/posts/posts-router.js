d3fff955168313947241575caf6b32f231301884
Amethyst Willia <zmithic@gmail.com>
2022-02-08 13:37:53 -0600

    Imporoperly implemented put and delete but went back to fix post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -53,16 +53,54 @@ router.get('/:id/comments', (req, res) => {
 // [POST] requests
 router.post('/', (req, res) => {
     const { title, contents } = req.body;
-    Post.insert({ title, contents })
+    if (!title || !contents) {
+        res.status(400).json({ message: 'you fucked up' });
+    } else {
+        Post.insert({ title, contents })
+        .then(({ id }) => {
+            return Post.findById(id);
+        })
         .then(post => {
-            if (title === undefined || contents === undefined) {
-                res.status(400).json({ message: "Please provide title and contents for the post" });
-            } else {
-                res.status(201).json(post);
-            }
+            res.status(201).json(post);
         })
         .catch(() => {
             res.status(500).json({ message: "There was an error while saving the post to the database" });
+        });
+    }
+});
+
+// [PUT] requests
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, contents } = req.body;
+    Post.update(id, { title, contents })
+        .then(post => {
+            if (post === null || post === undefined) {
+                res.status(404).json({ message: "The post with the specified ID does not exist" });
+            } else if (title === undefined || contents === undefined) {
+                res.status(400).json({ message: "Please provide title and contents for the post" });
+            } else {
+                res.status(200).json(post);
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ message: "The post information could not be modified" });
+        });
+});
+
+// [DELETE] requests
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    Post.remove(id)
+        .then(delPost => {
+            if (delPost === null || delPost === undefined) {
+                res.status(404).json({ message: "The post with the specified ID does not exist" });
+            } else {
+                res.status(200).json(delPost);
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ message: "The comments information could not be retrieved" });
         });
 });
 
