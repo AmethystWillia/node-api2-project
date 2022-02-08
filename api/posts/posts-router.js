ff58095bb1f2873baa478d32c790e08de709ae3e
Amethyst Willia <zmithic@gmail.com>
2022-02-08 12:58:57 -0600

    Added get request to get comments on a post based on id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -29,4 +29,25 @@ router.get('/:id', (req, res) => {
         });
 });
 
+router.get('/:id/comments', (req, res) => {
+    const { id } = req.params;
+    Post.findById(id)
+        .then(post => {
+            if (post === null || post === undefined) {
+                res.status(404).json({ message: "The post with the specified ID does not exist" });
+            } else {
+                Post.findPostComments(id)
+                    .then(post => {
+                        res.status(200).json(post);
+                    })
+                    .catch(() => {
+                        res.status(500).json({ message: "The comments information could not be retrieved" });
+                    });
+                }
+        })
+        .catch(() => {
+            res.status(500).json({ message: "The comments information could not be retrieved" });
+        });
+    });
+
 module.exports = router;
